8a183a07b87f9f909df533fdcc46250b33ceaa54
Alexander <alexanderdimitrov333@gmail.com>
2020-02-20 23:20:27 +0200

    Finished building the app

--- MovieTheather/assets/js/script.js
+++ MovieTheather/assets/js/script.js
@@ -1,6 +1,14 @@
  var buttarr1 = [];
  var numberOfSeats = 0;
  var finalPrice = 0;
+ var movieInfo ={
+     title : "",
+     duration : "",
+     description : "",
+     poster : "",
+     score : "",
+     date : ""
+ }
 //Create the three types of images
     //create the texts for the legend
     var legendTextOne = document.createElement('h5');
@@ -249,12 +257,13 @@ for (let index = 1; index <= 6; index++) {
 }
 $(document).on('click', '.seat-button', function() {
         var option = $("#select").val();
+        price = 0;
         console.log("return: "+option);
         switch(option){
             case "Avengers: Endgame":
                 price= 8;
                 break;
-            case "Fast and Furious":
+            case "Predestination":
                 price= 11;
                 break;
             case "Harry Potter":
@@ -265,6 +274,9 @@ $(document).on('click', '.seat-button', function() {
                 break;
             case "The curious case of Benjamin Button":
                 price= 10;
+                break;
+            default:
+                    price = 20;
                 break;
         }
         var currentId = this.id;
@@ -291,26 +303,56 @@ $(document).on('click', '.reservedSeat', function() {
 $( "#select" ).change(function() {
     removeSeats();
     numberOfSeats=0;
+    price=0;
     finalPrice= 0;
     $("#number-of-seats").html(numberOfSeats);
     $("#price").html(finalPrice);
     placeSeats();
-    displayMovie();
+
+  var movie = $("#select").val();
+    displayMovie(movie);
   });
 
-  function displayMovie(){
-    var movie = $("#select").val();
-    console.log("Movie is: " + movie);
-    
+  function displayMovie(movie){
     $.ajax({
         url: "https://api.themoviedb.org/3/search/movie?api_key=2774e8d9cf7be7186f2dcd76f02a37a7&query="+movie,
         method: 'GET',
         dataType: 'JSON',
         success: function(data) {
-          console.log(data)
+            console.log(data);
+          var movie = data.results[0];
+          movieInfo.title = movie.title;
+          movieInfo.description = movie.overview;
+          movieInfo.poster = movie.poster_path;
+          movieInfo.score = movie.vote_average;
+          movieInfo.date = movie.release_date;
+          console.log("Name is: " + movieInfo.title);
+          console.log("description: "+ movieInfo.description);
+          console.log("poster-Url"+ movieInfo.poster);
+          console.log("Score"+ movieInfo.score);
+          console.log("date:"+ movieInfo.date);
+          setMovie(movieInfo.title,movieInfo.poster,movieInfo.description,movieInfo.score,movieInfo.date)
         },
         error: function(err) {
           console.log('error:' + err)
         }
       })
   }
+
+  function setMovie(title,image,description,score,date){
+      $("#movie-name-field").html(title);
+      $("#date-field").html(date);
+      $("#movie-image-field").css('background-image', 'url("http://image.tmdb.org/t/p/w185/'+image+'")');
+      console.log("http://image.tmdb.org/t/p/w185/"+ image);
+      $("#desc-field").html(description);
+      $("#score-field").html(score);
+  }
+
+  $("#search-butt").click(function(){
+      var title = $("#search-text").val();
+      displayMovie(title);
+      removeSeats();
+      placeSeats();
+      $("#opt-6").html(title);
+      console.log("Hello"+$("#select").prop("selectedIndex", 0).val());
+  })
